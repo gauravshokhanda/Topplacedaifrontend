5a7bb7a016f9f89590c344a1399caa6da900275b
Gaurav Shokhanda <gauravshokanda2@gmail.com>
2025-07-31 23:41:27 +0530

    Fix Static Export and API Configuration Errors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  optimizeFonts: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NODE_ENV === "production"
