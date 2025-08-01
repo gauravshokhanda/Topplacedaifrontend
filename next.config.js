7f32490cdf5374991be548dddba14fa66327687c
Gaurav Shokhanda <gauravshokanda2@gmail.com>
2025-08-01 12:08:29 +0530

    Configure Pexels domain for Next.js images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['images.pexels.com'],
+  },
   env: {
     NEXT_PUBLIC_API_URL:
       process.env.NODE_ENV === "production"
