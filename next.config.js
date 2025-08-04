e6b126ece975b94fea594144bcba77417be4fbeb
Gaurav Shokhanda <gauravshokanda2@gmail.com>
2025-08-04 23:52:55 +0530

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL:
       process.env.NODE_ENV === "production"
         ? "YOUR_PRODUCTION_API_URL"
-        : "https://d312b60796ea.ngrok-free.app",
+        : "https://a67ae9e25cd7.ngrok-free.app",
   },
 };
 
